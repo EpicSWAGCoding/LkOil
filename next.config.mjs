8cc1b3a4fbbafb08dedad114efeff5d9ce277279
Egor <EpicSWAG123@yandex.ru>
2024-09-26 11:49:22 +0300

    FastFix9.9.24

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+    reactStrictMode: true,
+    env: {
+        JWT_SECRET: process.env.JWT_SECRET,
+        ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+        POSTGRES_URL: process.env.POSTGRES_URL,
+        POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING
+    },
 };
 
 export default nextConfig;
