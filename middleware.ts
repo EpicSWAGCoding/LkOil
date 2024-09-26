7fb940a56a760763d29a86e97bffd50fee823c24
Egor <EpicSWAG123@yandex.ru>
2024-09-26 15:14:28 +0300

    FastFix9.9.26

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,20 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
     const token = req.cookies.get('token')?.value;
 
-    // Разрешить доступ к странице авторизации, если нет токена
+    // Если токен существует, перенаправляем на главную страницу
+    if (token) {
+        // Перенаправляем авторизованных пользователей на главную страницу
+        const url = req.nextUrl.clone();
+        url.pathname = '/'; // или укажите нужный путь для авторизованных пользователей
+        return NextResponse.redirect(url);
+    }
+
+    // Разрешить доступ к страницам авторизации, если нет токена
     if (req.nextUrl.pathname.startsWith('/auth') || req.nextUrl.pathname === '/not-auth') {
         return NextResponse.next();
     }
 
-    // Если токен не найден, перенаправляем на страницу "not-auth"
+    // Если токен не найден и пользователь пытается попасть на защищенные страницы, перенаправляем на "not-auth"
     if (!token) {
         const url = req.nextUrl.clone();
         url.pathname = '/not-auth';
@@ -22,5 +30,5 @@ export function middleware(req: NextRequest) {
 
 // Указываем пути, для которых нужно применять middleware
 export const config = {
-    matcher: ['/', '/profile/:path*', '/card/:path*', '/billing/:path*', '/refill/:path*'], // Здесь указаны страницы, где требуется проверка
+    matcher: ['/', '/profile/:path*', '/card/:path*', '/billing/:path*', '/refill/:path*', '/not-auth'], // Добавлено '/not-auth' в matcher
 };
