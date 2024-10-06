00e8c860a7ecc0dac2bbee132d52ff703d92c352
Egor <EpicSWAG123@yandex.ru>
2024-10-06 12:02:15 +0300

    fix 1.1.2

--- app/api/limitsset/route.ts
+++ app/api/limitsset/route.ts
@@ -1,24 +1,24 @@
-// // app/api/limits/set/route.ts
-// import { NextResponse } from 'next/server';
-// import { prisma } from '@/prisma/prisma-client';
-//
-// export async function POST(request: Request) {
-// 	const { fuelType, limit, driverName, carNumber, cardId } = await request.json();
-//
-// 	try {
-// 		const newLimit = await prisma.limit.create({
-// 			data: {
-// 				fuelType,
-// 				limit: parseFloat(limit), // Убедитесь, что лимит число
-// 				driverName,
-// 				carNumber,
-// 				cardId,
-// 			},
-// 		});
-//
-// 		return NextResponse.json(newLimit, { status: 201 });
-// 	} catch (error) {
-// 		console.error('Error creating limit:', error);
-// 		return NextResponse.json({ error: 'Failed to create limit' }, { status: 500 });
-// 	}
-// }
+// app/api/limits/set/route.ts
+import { NextResponse } from 'next/server';
+import { prisma } from '@/prisma/prisma-client';
+
+export async function POST(request: Request) {
+	const { fuelType, limit, driverName, carNumber, cardId } = await request.json();
+
+	try {
+		const newLimit = await prisma.limit.create({
+			data: {
+				fuelType,
+				limit: parseFloat(limit), // Убедитесь, что лимит число
+				driverName,
+				carNumber,
+				cardId,
+			},
+		});
+
+		return NextResponse.json(newLimit, { status: 201 });
+	} catch (error) {
+		console.error('Error creating limit:', error);
+		return NextResponse.json({ error: 'Failed to create limit' }, { status: 500 });
+	}
+}
